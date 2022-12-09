05335f13fb654149444bf3dca13d904af4bc1679
wubeshetA <w.yimam@alustudent.com>
2022-12-09 11:01:00 +0300

    hot fix

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,10 @@
 # Increase the number of requests nginx can handle.
-exec { 'fix-requests-handler':
-    command => 'sed -i "s/15/4096/" /etc/default/nginx',
-    path => '/usr/local/bin/:/bin/'   
-}
+exec { 'fix-requests-handler' }':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
 
 exec { 'restart-nginx':
-    command => 'nginx restart',
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
