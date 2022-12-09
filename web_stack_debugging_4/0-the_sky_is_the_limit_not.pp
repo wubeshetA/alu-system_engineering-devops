5d3dc052c3b038a48ea2990a68ac3d56d888d47b
wubeshetA <w.yimam@alustudent.com>
2022-12-09 10:54:19 +0300

    restart nginx

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,9 @@
 # Increase the number of requests nginx can handle.
-exec {
+exec { 'fix-requests-handler':
     command => 'sed -i "s/15/4096/" /etc/default/nginx',
     path => '/usr/local/bin/:/bin/'   
+}
+
+exec { 'restart-nginx':
+    command => 'nginx restart',
 }
